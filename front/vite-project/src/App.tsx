a8f5f9ebefd1d4550bfb0620e723d001cadfdeb2
Prudhvi Kothakonda <prudhvikthknda@gmail.com>
2023-10-25 15:31:17 -0500

    small error fix in app.js

--- front/vite-project/src/App.tsx
+++ front/vite-project/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from './Menu'
 import Manager from './Manager'
+import Login from './Login'
 
 interface Props {
   name: string;
@@ -25,7 +26,7 @@ function App() {
         <Menu items={menuItems} />
         <Routes>
           <Route path="/manager" element={<Manager name={name} age={age} employees={employees} />} />
-          <Route path="/" element={/<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </BrowserRouter>

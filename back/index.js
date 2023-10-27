ee36fc415f1610d0e51ba3931383204b95a249b1
samdcole48 <samdcole48@gmail.com>
2023-10-27 00:37:08 -0500

    example post request

--- back/index.js
+++ back/index.js
@@ -1,14 +1,32 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
+
+app.use(express.json());
 
 // when "get /arg" is called this function will be executed
 // req is incoming data, response is data we want to send back to client
 // try GET http://localhost:8080/arg on postman
-app.get('/arg', (req, res) => {
+app.get('/tshirt', (req, res) => {
     res.status(200).send({
-        example1: 'test1',
-        example2: 'test2',
+        tshirt: 'red',
+        size: 'large',
     })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!'})
+    }
+
+    res.send({
+        tshirt: `tshirt with your ${logo} and ID of ${id}`,
+    });
+
 });
 
 app.listen(

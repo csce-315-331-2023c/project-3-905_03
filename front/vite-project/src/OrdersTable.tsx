7639bdfe77e727e1de0a852f3588da6ea724b20b
revmya09 <113645178+revmya09@users.noreply.github.com>
2023-10-28 21:11:34 -0500

    initialized table for orders panel; added view button (to be implemented later)

--- front/vite-project/src/OrdersTable.tsx
+++ front/vite-project/src/OrdersTable.tsx
@@ -1,94 +1,52 @@
 import React, {useState} from 'react';
 import "./Table.css";
-import {BsFillPencilFill, BsFillTrashFill} from 'react-icons/bs'
+import {BsFillTrashFill, BsEyeFill} from 'react-icons/bs'
 
 function OrdersTable() {
     const [rows, setRows] = useState([
-        {employee_id: 13, order_id: 0, order_total: 41.19, takeout: "39", split: "120", order_date: "2023-02-01 20:29:21"},
-        {employee_id: 0, order_id: "chicken", order_total: "6.95", takeout: "39", split: "120", order_date: "2023-02-01 20:29:21"},
-        {employee_id: 0, order_id: "chicken", order_total: "6.95", takeout: "39", split: "120", order_date: "2023-02-01 20:29:21"}
+        {employee_id: 13, order_id: 0, order_total: "41.19", takeout: "1", split: "1", order_date: "2023-02-01 20:29:21"},
+        {employee_id: 13, order_id: 1, order_total: "47.52", takeout: "1", split: "1", order_date: "2023-01-31 15:49:13"},
+        {employee_id: 9, order_id: 2, order_total: "63.81", takeout: "0", split: "1", order_date: "2023-03-11 07:34:36"}
     ]);
-    const [editId, setEditId] = useState(-1);
-    const [name, setName] = useState('');
-    const [cost, setCost] = useState('');
-    const [quantity, setQuantity] = useState('');
-    const [maxAmount, setMaxAmount] = useState('');
+    const [employeeID, setEmployeeID] = useState(-1);
+    const [orderID, setOrderID] = useState(-1);
+    const [orderTotal, setOrderTotal] = useState('');
+    const [takeout, setTakeout] = useState('');
+    const [split, setSplit] = useState('');
+    const [orderDate, setOrderDate] = useState('');
     const [rowToEdit, setRowToEdit] = useState(null);
     
     const handleDeleteRow = (targetIndex: number) => {
         setRows(rows.filter((_, idx) => idx !== targetIndex))
     };
-
-    const handleEditRow = (stock_id: number) => {
-        rows.map((row) => {
-            if (row.stock_id === stock_id){
-                setName(row.stock_item)
-                setCost(row.cost)
-                setQuantity(row.stock_quantity)
-                setMaxAmount(row.max_amount)
-            }
-        })
-        setEditId(stock_id)
-    }
-
-    const handleUpdate = () => {
-        const updatedRows = rows.map((row) => {
-            if (row.stock_id === editId){
-                return {
-                    ...row,
-                    served_item: name,
-                    cost: cost,
-                    stock_quantity: quantity,
-                    max_amount: maxAmount
-                };
-            }
-            return row;
-        });
-
-        setRows(updatedRows);
-        setEditId(-1);
-        setName('')
-        setCost('')
-        setQuantity('')
-        setMaxAmount('')
-    }
-
     
     return (
         <div className='table-container'>
             <table className='table'>
                 <thead>
                     <tr>
-                        <th>Stock ID</th>
-                        <th className='expand'>Stock Name</th>
-                        <th>Cost</th>
-                        <th>Quantity</th>
-                        <th>Maximum Amount</th>
+                        <th>Employee ID</th>
+                        <th>Order ID</th>
+                        <th>Order Total</th>
+                        <th>Takeout</th>
+                        <th>Split</th>
+                        <th className='expand'>Order Date</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
                         rows.map((row, idx) => (
-                            row.stock_id === editId ?
-                            <tr>
-                                <td>{row.stock_id}</td>
-                                <td><input type="text" value={name} onChange={e => setName(e.target.value)}/></td>
-                                <td><input type="text" value={cost} onChange={e => setCost(e.target.value)}/></td>
-                                <td><input type="text" value={quantity} onChange={e => setQuantity(e.target.value)}/></td>
-                                <td><input type="text" value={maxAmount} onChange={e => setMaxAmount(e.target.value)}/></td>
-                                <td><button onClick={handleUpdate}>Update</button></td>
-                            </tr>
-                            :
                             <tr key={idx}>
-                                <td>{row.stock_id}</td>
-                                <td className='expand'>{row.stock_item}</td>
-                                <td>{row.cost}</td> 
-                                <td>{row.stock_quantity}</td>
-                                <td>{row.max_amount}</td>
+                                <td>{row.employee_id}</td>
+                                <td>{row.order_id}</td>
+                                <td>{row.order_total}</td> 
+                                <td>{row.takeout}</td>
+                                <td>{row.split}</td>
+                                <td className='expand'>{row.order_date}</td>
                                 <td>
                                     <span className='actions'>
-                                        <BsFillPencilFill className="edit-btn" onClick={() => handleEditRow(row.stock_id)}/>
+                                        <BsEyeFill className="view-btn"/>
                                         <BsFillTrashFill className="delete-btn" onClick={() => handleDeleteRow(idx)}/>
                                     </span>
                                 </td>

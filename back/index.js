f79c1d66436ec1399e1de9164a6f5666684a533e
samdcole48 <samdcole48@gmail.com>
2023-11-08 14:08:42 -0600

    added in submitOrder funcitonality from sam-submirOrder branch

--- back/index.js
+++ back/index.js
@@ -407,6 +407,67 @@ app.post('/deleteStockItem', (req, res) => {
     });
 });
 
+/**
+ * given employee_id (for now assume is given to you), a list of item ids, order total, takeout, and split, submit the order to the database
+ * get max order id
+ * get the date
+ * add to orders table
+ * for each item add to orderserveditems table
+ * for each item in the list of item_ids, get the ingredients used from the serveditemstockitem table
+ * for each of these ingredients decrement the stock_quantity in the stock_items table
+ */
+app.post('/submitOrder', async (req, res) => { 
+    let client;
+    
+    try {
+        let { item_list, employee_id, order_total, takeout, split} = req.body;
+
+        client = new Client({
+            host: 'csce-315-db.engr.tamu.edu',
+            user: 'csce315_905_03user',
+            password: '90503',
+            database: 'csce315_905_03db'
+        });
+
+        await client.connect();
+
+        const maxOrderIdResult = await client.query('SELECT MAX(order_id) FROM orders');
+        let maxorderId = maxOrderIdResult.rows[0].max || 0;
+        let neworderId = maxorderId + 1;
+
+        const currentDate = new Date();
+        const formattedDate = currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1).toString().padStart(2, '0') + '-' + currentDate.getDate().toString().padStart(2, '0');
+        const formattedTime = currentDate.getHours().toString().padStart(2, '0') + ':' + currentDate.getMinutes().toString().padStart(2, '0') + ':' + currentDate.getSeconds().toString().padStart(2, '0');
+        const dateTime = formattedDate + ' ' + formattedTime;
+
+        await client.query('INSERT INTO orders (employee_id, order_id, order_total, takeout, split, order_date) VALUES ($1, $2, $3, $4, $5, $6)', [employee_id, neworderId, order_total, takeout, split, dateTime]);
+
+        const maxOrderItemIdResult = await client.query('SELECT MAX(order_item_id) FROM orderserveditem');
+        let maxOrderItemId = maxOrderItemIdResult.rows[0].max || 0;
+        let newOrderItemId = maxOrderItemId + 1;
+
+        for (const item of item_list) {
+            await client.query('INSERT INTO orderserveditem (order_id, item_id, order_item_id) VALUES ($1, $2, $3)', [neworderId, item, newOrderItemId]);
+
+            const stock_ids_usedResult = await client.query('SELECT stock_id FROM serveditemstockitem WHERE item_id = $1', [item]);
+            const stock_ids_used = stock_ids_usedResult.rows;
+
+            for (const stock_id of stock_ids_used) {
+                await client.query('UPDATE stock_items SET stock_quantity = stock_quantity - 1 WHERE stock_id = $1', [stock_id.stock_id]);
+            }
+            newOrderItemId++;
+        }
+
+        res.status(200).send('success!');
+    } catch (error) {
+        res.status(400).send(error.message);
+    } finally {
+        if (client) {
+            client.end();
+        }
+    }
+});
+
 //// SERVER
 
 app.get('*', (req, res) => {

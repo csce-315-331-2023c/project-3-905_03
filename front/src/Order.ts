90cf7b60a481ca59e22ca3850abe7650708b670c
revmya09 <113645178+revmya09@users.noreply.github.com>
2023-11-10 18:10:22 -0600

    minor changes

---
+++ front/src/Order.ts
@@ -0,0 +1,42 @@
+interface Item {
+    id: number;
+    name: string;
+    price: number;
+    quantity: number;
+  }
+  
+  export class Order {
+    private receipt: Item[] = [];
+  
+    addItem(id: number, name: string, price: number, quantity: number): void {
+      const existingItem = this.receipt.find((item) => item.id === id);
+      if (existingItem) {
+        existingItem.quantity += quantity;
+      } else {
+        const item: Item = { id, name, price, quantity };
+        this.receipt.push(item);
+      }
+    }
+  
+    removeItem(id: number): void {
+      const existingItem = this.receipt.find((item) => item.id === id);
+      if (existingItem) {
+        existingItem.quantity -= 1;
+        if (existingItem.quantity === 0) {
+          this.receipt = this.receipt.filter((item) => item.id !== id);
+        }
+      }
+    }
+  
+    cancel(): void {
+      this.receipt = [];
+    }
+  
+    checkout(): void {
+      // implementation for checkout
+    }
+  
+    getReceipt(): string {
+      return JSON.stringify(this.receipt);
+    }
+  }

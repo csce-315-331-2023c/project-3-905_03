d4394d97ae2661086bb7dca83bd83a487a5fbae6
Prudhvi Kothakonda <prudhvikthknda@gmail.com>
2023-11-15 14:16:08 -0600

    formatted order with no quantity

--- front/src/Order.ts
+++ front/src/Order.ts
@@ -3,67 +3,51 @@ export interface Item {
   id: number;
   name: string;
   price: number;
-  quantity : number;
-  category : string;
-  description ?: string;
+  category: string;
+  description?: string;
 }
 
 export class Order {
   private receipt: Item[] = [];
 
-  constructor() {}
+  constructor() { }
 
-  addItem(id: number, name: string, price: number, quantity: number, category: string): void {
-    const existingItem = this.receipt.find((item) => item.id === id);
-    if (existingItem) {
-      existingItem.quantity += quantity;
-    } else {
-      const item: Item = { id, name, price, quantity, category };
-      this.receipt.push(item);
-    }
+  addItem(adding: Item): void {
+    this.receipt.push(adding);
   }
-  
-    removeItem(id: number): void {
-      const existingItem = this.receipt.find((item) => item.id === id);
-      if (existingItem) {
-        existingItem.quantity -= 1;
-        if (existingItem.quantity === 0) {
-          this.receipt = this.receipt.filter((item) => item.id !== id);
-        }
-      }
-    }
 
-    deleteItem(id: number): void {
-      this.receipt = this.receipt.filter((item) => item.id !== id);
-    }
 
-    getOrderTotal(): string {
-      const orderTotal = this.receipt.reduce((total, item) => total + item.price * item.quantity, 0);
-      return orderTotal.toFixed(2);
-    }
-
-    splitOrder(): string {
-      const orderTotal = this.receipt.reduce((total, item) => total + item.price * item.quantity, 0);
-      return (orderTotal / 2).toFixed(2);
-    }
-  
-    cancel(): void {
-      this.receipt = [];
-    }
-  
-    checkout(): void {
-      // implementation for checkout
-    }
-  
-    getReceipt(): string {
-      return JSON.stringify(this.receipt);
-    }
-
-    getReceipt2(): Item[] {
-        return this.receipt;
-    }
-
-    setReceipt(receipt: Item[]): void {
-        this.receipt = receipt;
-    }
+  removeItem(removing: Item): void {
+    this.receipt = this.receipt.filter((item) => item !== removing);
   }
+
+  getOrderTotal(): string {
+    const orderTotal = this.receipt.reduce((total, item) => total + item.price, 0);
+    return orderTotal.toFixed(2);
+  }
+
+  splitOrder(): string {
+    const orderTotal = this.receipt.reduce((total, item) => total + item.price, 0);
+    return (orderTotal / 2).toFixed(2);
+  }
+
+  cancel(): void {
+    this.receipt = [];
+  }
+
+  checkout(): void {
+    // implementation for checkout
+  }
+
+  getReceipt(): string {
+    return JSON.stringify(this.receipt);
+  }
+
+  getReceipt2(): Item[] {
+    return this.receipt;
+  }
+
+  setReceipt(receipt: Item[]): void {
+    this.receipt = receipt;
+  }
+}

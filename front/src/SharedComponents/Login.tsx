6c23e9e662852ad6168028ff97efb92424f96913
Prudhvi Kothakonda <prudhvikthknda@gmail.com>
2023-12-04 13:26:41 -0600

    temp disable google login

--- front/src/SharedComponents/Login.tsx
+++ front/src/SharedComponents/Login.tsx
@@ -203,7 +203,7 @@ const LoginPage = () => {
             <button className="login-button" onClick={handleManualLoginSubmit}>
               Submit
             </button>
-            <div className="google-auth">
+            {/* <div className="google-auth">
               <GoogleLogin
                 onSuccess={handleGoogleLoginSuccess}
                 onError={handleGoogleLoginError}
@@ -213,8 +213,8 @@ const LoginPage = () => {
                 size='large'
                 logo_alignment='center'
                 width={250}
-              />
-            </div>
+              /> 
+            </div>*/}
           </div>
           <Divider orientation="vertical" flexItem sx={{backgroundColor:'#ffffff'} } />
           <div className="guest-options">

e2b0efa4afa19cf0bad2f7db6ab3e0604f8a7ad5
samdcole48 <samdcole48@gmail.com>
2023-12-05 13:28:34 -0600

    fix menu SpecialItems object and CustomerMenu css

--- front/src/DynamicMenu/Components/SpecialItems.tsx
+++ front/src/DynamicMenu/Components/SpecialItems.tsx
@@ -1,43 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {formatCamelCase} from '../../SharedComponents/itemFormattingUtils.ts';
-
-interface Row {
-    item_id: number;
-    served_item: string;
-    item_price: number;
-    family_id: 0;
-}
-
-interface Data {
-    data: Row[];
-}
+import ItemsInFamily from './ItemsInFamily.tsx';
 
 const SpecialItems: React.FC = () => {
-    const [items, setItems] = useState<Row[]>([]);
+    interface Row {
+        family_id: number;
+        family_name: string;
+        family_category: string;
+        family_description: string;
+    }
 
-    useEffect(() => {
+    interface Data {
+        data: Row[];
+    }
+
+    const [rows, setRows] = useState<Row[]>([]); // Use Row[] instead of any[]
+
+    const fetchSpecialItems = () => {
+        axios.get('/getSpecialItems')
+            .then(res => {
+                const data: Data = res.data;
+                setRows(data.data);
+            })
+            .catch(err => console.log(err));
+    };
     
-        axios.post('/getServedItemsInFamily', { "family_id" : 37 })
-        .then(res => {
-            const data: Data = res.data;
-            setItems(data.data);
-        })
-        .catch(err => {
-            console.log('Error:', err); // Log any errors from the axios.get call
-        });
-    });
-
+    useEffect(() => {
+        fetchSpecialItems();
+    }, []);
+    
     return (
-        <span>
-            {items && items.map((item, index) => (
-            <div className = "menuItems" key={item.item_id}>
-                <div className="menuItem">
-                    {formatCamelCase(item.served_item)} : {item.item_price}
+        <div className = "menuItems">
+            {rows.map((row, index) => (
+                <div key={index} className="menuItem">
+                    <span>{row.family_name} <ItemsInFamily family_id={row.family_id}/></span>
+                    <div className = "menuItemDescription">{row.family_description}</div>
                 </div>
-            </div>
-        ))}
-        </span>
+            ))}
+        </div>
     );
 };
 

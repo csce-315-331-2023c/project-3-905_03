278933ad30681fab4d6eb32ef0808b15aad89f03
Ryan Tri <ry4ntr1@gmail.com>
2023-12-11 16:52:03 -0600

    Manual Login

--- back/manualAuth.js
+++ back/manualAuth.js
@@ -29,51 +29,49 @@ const pool = new Pool({
 router.post('/auth/manual/login', async (req, res) => {
     const { email, password } = req.body;
     try {
-        const query = 'SELECT * FROM employees WHERE email = $1;';
-        const dbRes = await pool.query(query, [email]);
-        const user = dbRes.rows[0];
+        const employeeQuery = 'SELECT * FROM employees WHERE email = $1';
+        const employeeRes = await pool.query(employeeQuery, [email]);
+        const employee = employeeRes.rows[0];
 
-        const customerQuery = 'SELECT * FROM customers WHERE email = $1';
-        const dbCustomerRes = await pool.query(customerQuery, [email]);
-        const customerUser = dbCustomerRes.rows[0];
-
-        if (user) {
-            const passwordMatch = await bcrypt.compare(password, user.password);
-            if (passwordMatch) {
-                const userForToken = {
-                    email: user.email,
-                    firstName: user.first_name,
-                    lastName: user.last_name,
-                    role: user.role,
-                    profilePic: user.profile_pic,
-                };
-
-                const token = jwt.sign(userForToken, process.env.JWT_SECRET, { expiresIn: '1h' });
-                return res.status(200).json({ token });
-            } 
+        let customer = null;
+        if (!employee) {
+            const customerQuery = 'SELECT * FROM customers WHERE email = $1';
+            const customerRes = await pool.query(customerQuery, [email]);
+            customer = customerRes.rows[0];
         }
 
-        else if (customerUser) {
-            const passwordMatch = await bcrypt.compare(password, customerUser.password);
+        if (employee) {
+            const passwordMatch = await bcrypt.compare(password, employee.password);
+            if (passwordMatch) {
+                const userForToken = {
+                    email: employee.email,
+                    firstName: employee.first_name,
+                    lastName: employee.last_name,
+                    role: employee.role,
+                    profilePic: employee.profile_pic,
+                };
+                const token = jwt.sign(userForToken, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+
+                return res.status(200).json({ token });
+            }
+        } else if (customer) {
+            const passwordMatch = await bcrypt.compare(password, customer.password);
             if (passwordMatch) {
                 const customerUserToken = {
-                    firstName: customerUser.first_name,
-                    lastName: customerUser.last_name,
+                    email: customer.email,
+                    firstName: customer.first_name,
+                    lastName: customer.last_name,
                     role: 'customer',
-                    email: customerUser.email,
-                    profilePic: customerUser.profile_pic,
-                }
-
+                    profilePic: customer.profile_pic,
+                };
                 const token = jwt.sign(customerUserToken, process.env.JWT_SECRET, { expiresIn: '1h' });
                 return res.status(200).json({ token });
             }
         }
-        else {
-            return res.status(404).json({ message: 'Manual Log-In: No User Found' });
-        }
+        return res.status(401).json({ message: 'Invalid Credentials' });
     } catch (error) {
-        console.error('Login Error:', error);
-        return res.status(500).json({ message: 'Manual Log-In: Server Connection Cannot Be Established' });
+        return res.status(500).json({ message: 'Internal Server Error' });
     }
 });
 
